0d8937c3b7b836f06eac3255dc038c3bffbd7476
Dario Castaño <dario.castano@aim.com>
2019-09-10 22:12:55 -0500

    add answer to the 13th task

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,12 +1,58 @@
 #include "lists.h"
 
 /**
- * insert_node - 
- * @head:
- * @number:
- * Return:
+ * insert_node - insert node in a ordered linkedlist
+ * @head: the head node
+ * @number: number to insert
+ * Return: Address of the new node, otherwise NULL
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+	listint_t *new_node;
+	listint_t *left = NULL; 
+	listint_t *right = NULL;
 
+	if(head == NULL)
+		return(NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	right = *head;
+	
+	if ((left == NULL) && (new_node->n <= right->n))
+	{
+		new_node->next = right;
+		*head = new_node;
+		return (new_node);
+	}
+
+	right = right->next;
+	left = *head; 
+
+	while (right != NULL)
+	{
+		if ((left->n <= new_node->n) && (new_node->n <= right->n))
+		{
+			new_node->next = right;
+			left->next = new_node;
+			return (new_node);
+		}
+		
+		right = right->next;
+		left = left->next;
+	}
+	
+	left->next = new_node;
+	return (new_node);
 }
