dc259996250871273539bbc52d8a449445d61e1b
Dario <dario.castano@aim.com>
2019-09-11 03:26:09 +0000

    Fix betty on task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,38 +8,29 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node;
-	listint_t *left = NULL; 
-	listint_t *right = NULL;
+	listint_t *new_node, *left = NULL, *right = NULL;
 
-	if(head == NULL)
-		return(NULL);
-
+	if (head == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = number;
 	new_node->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-
 	right = *head;
-	
 	if ((left == NULL) && (new_node->n <= right->n))
 	{
 		new_node->next = right;
 		*head = new_node;
 		return (new_node);
 	}
-
 	right = right->next;
-	left = *head; 
-
+	left = *head;
 	while (right != NULL)
 	{
 		if ((left->n <= new_node->n) && (new_node->n <= right->n))
@@ -48,11 +39,9 @@ listint_t *insert_node(listint_t **head, int number)
 			left->next = new_node;
 			return (new_node);
 		}
-		
 		right = right->next;
 		left = left->next;
 	}
-	
 	left->next = new_node;
 	return (new_node);
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -13,8 +13,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -24,3 +24,4 @@ void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
+
