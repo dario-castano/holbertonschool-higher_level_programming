72f176aa1c696ccb5dda7a1b6b94def46535a4ee
Dario Castaño <dario.castano@aim.com>
2019-09-13 01:39:56 +0000

    Add exception handling on value errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,25 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     argc = len(argv)
-    opers = ["+", "-", "*", "/"]
-    if argc < 4 or argc > 4:
+    if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        a = int(argv[1])
-        operator = argv[2]
-        b = int(argv[3])
+        try:
+            a = int(argv[1])
+            operator = argv[2]
+            b = int(argv[3])
+        except ValueError:
+            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+            exit(1)
 
-        if operator is "+":
+        if ord(operator) == 43:
             print(add(a, b))
-        elif operator is "-":
+        elif ord(operator) == 45:
             print(sub(a, b))
-        elif operator is "*":
+        elif ord(operator) == 42:
             print(mul(a, b))
-        elif operator is "/":
+        elif ord(operator) == 47:
             print(div(a, b))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
