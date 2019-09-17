c3f13e5dfef5fd327c9e88a65a58520c83e5e8d2
Dario Castaño <dario.castano@aim.com>
2019-09-17 17:03:32 -0500

    Fix pep8 on 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    str_without_cs = "".join([x for x in my_string if x is not 'C' and x is not 'c'])
-    return str_without_cs
+    newstr = "".join([x for x in my_string if x is not 'C' and x is not 'c'])
+    return newstr
