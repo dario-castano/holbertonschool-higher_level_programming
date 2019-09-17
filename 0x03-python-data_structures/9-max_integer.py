b88d39d68d42bbe8114aa8045b4862019fd2ab9b
Dario Castaño <dario.castano@aim.com>
2019-09-17 18:24:44 -0500

    Add 9-max_integer.py to the repo

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        max = my_list[0]
+        for n in my_list:
+            if n > max:
+                max = n
+        return max
+        
