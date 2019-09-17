8f1304eeb9a11f8709e692afae8eb8540c41a62d
Dario Castaño <dario.castano@aim.com>
2019-09-17 15:36:55 -0500

    Check empty list on 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    return [print("{:d}".format(x)) for x in reversed(my_list)]
+    if not my_list:
+        return None
+    else:
+        return [print("{:d}".format(x)) for x in reversed(my_list)]
