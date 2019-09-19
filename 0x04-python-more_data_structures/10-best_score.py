2e2ce070337bcf427007a2d6f87f2bfd8405fe5e
Dario Castaño <dario.castano@aim.com>
2019-09-19 14:14:27 -0500

    Fix 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     try:
-        return max(a_dictionary)
-    except TypeError:
+        winner = {'name': '', 'score': 0}
+        for key, value in a_dictionary.items():
+            if value > winner['score']:
+                winner.update({'name': key, 'score': value})
+            else:
+                pass
+        return winner['name']
+    except AttributeError:
         return None
