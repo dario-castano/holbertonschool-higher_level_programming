9a5a3aa6f876ed0dc970c061c95024bd240865d7
Dario Castaño <dario.castano@aim.com>
2019-09-19 15:08:19 -0500

    Implement new 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
+    if not a_dictionary:
+        pass
     else:
-        winner = {'name': '', 'score': 0}
-        for key, value in a_dictionary.items():
-            if value > winner['score']:
-                winner.update({'name': key, 'score': value})
-            else:
-                pass
-        return winner['name']
+        keys = list(a_dictionary.keys())
+        values = list(a_dictionary.values())
+        return keys[values.index(max(values))]
