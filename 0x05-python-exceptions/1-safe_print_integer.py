17dc57ff67fa26d14a12a83032eda12ae7b50740
Dario Castaño <dario.castano@aim.com>
2019-09-30 15:03:58 -0500

    Fix wrong function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    k = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            k += 1
-        except (TypeError, ValueError):
-            i -= 1
-    print()
-    return k
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
+        return False
+    return True
+
