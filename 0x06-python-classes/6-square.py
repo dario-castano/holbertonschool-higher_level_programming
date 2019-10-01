b31dc20cb69f79bce66e7eee01f9ea50ff8a79bc
Dario Castaño <dario.castano@aim.com>
2019-10-01 16:29:57 -0500

    Add task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Defines a square
+
+    Args:
+        size (int): Size of the square
+
+    Attributes:
+        __size (int): Size of the square
+
+    Raises:
+        ValueError: When size is less than 0
+        TypeError: Size must be an int
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Args:
+            size: Size of the square
+        """
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
+
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(self.size):
+                print('#' * self.size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Args:
+            value: Setter of size property
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple and isinstance(value, int):
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
