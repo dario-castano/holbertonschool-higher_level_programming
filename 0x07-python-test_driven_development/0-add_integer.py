49d8d93ed85f80ba19dc70b2c036a8ef737b1c1a
Dario Castaño <dario.castano@aim.com>
2019-10-05 11:55:01 -0500

    Fix NaN and overflow test on 0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,4 +5,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    if a == float(9e999) or a != a or a == float(-9e999):
+        raise TypeError("a must be an integer")
+    if b == float(9e999) or b != b or b == float(-9e999):
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
