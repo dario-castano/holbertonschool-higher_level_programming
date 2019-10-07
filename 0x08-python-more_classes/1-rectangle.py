fe139200a5445169c2ddd461e216eeb53432f53e
Dario Castaño <dario.castano@aim.com>
2019-10-07 11:45:00 -0500

    Add answers to mandatory tasks

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """WIDTH GETTER"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """WIDTH SETTER"""
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """HEIGHT GETTER"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """WIDTH SETTER"""
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,1 +1,73 @@
 #!/usr/bin/python3
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+        return
+
+    def __repr__(self):
+        return 'Rectangle({}, {})'.format(self.width, self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            word = []
+            for i in range(self.height):
+                if i == self.height - 1:
+                    word.append(str(self.print_symbol) * self.width)
+                else:
+                    word.append(str(self.print_symbol) * self.width + '\n')
+            return "".join(word)
+
+    @property
+    def width(self):
+        """WIDTH GETTER"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """WIDTH SETTER"""
+        if not type(value) is int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """HEIGHT GETTER"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """WIDTH SETTER"""
+        if not type(value) is int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
