b2bdcbfad745c0dc51ddea4a9557d809e21935e4
Dario Castaño <dario.castano@aim.com>
2019-10-08 10:08:00 -0500

    Fix typos on 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
             return "".join(word)
 
     def __eq__(self, other):
-        return self.area() > other.area()
+        return self.area() == other.area()
 
     def __gt__(self, other):
         return self.area() > other.area()
@@ -45,7 +45,7 @@ class Rectangle:
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         else:
             if rect_1 >= rect_2:
                 return rect_1
