f558f712e1f9e40a1ffef1b2f4c68a70925bc9cf
Dario Castaño <dario.castano@aim.com>
2019-10-08 10:12:07 -0500

    Add comment on 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -42,6 +42,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """Compares 2 areas of rectangles"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
