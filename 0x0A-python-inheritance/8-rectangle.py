3c09cfeb70c1f7e26086e0a46eb36f2da1bb4f2c
Dario Castaño <dario.castano@aim.com>
2019-10-15 16:00:36 -0500

    WIP: Add Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """Create a BaseGeometry object"""
+    def area(self):
+        """Area of the BaseGeometry object"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Integer validator"""
+        if not (type(value) is int):
+            raise TypeError(name + " must be an integer")
+        elif value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = self.integer_validator()
