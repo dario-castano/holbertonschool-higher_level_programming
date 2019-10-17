bd05259d19f43fca3505ce0eefed4bbe78ab830a
Dario Castaño <dario.castano@aim.com>
2019-10-16 20:58:46 -0500

    Fix error on 10-class_to_json.py

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import json
 
 
 def class_to_json(obj):
     """returns the dictionary description with simple data structure
      (list, dictionary, string, integer and boolean) for JSON serialization
       of an object"""
-    return json.loads(json.dumps(obj.__dict__))
+    return obj.__dict__
