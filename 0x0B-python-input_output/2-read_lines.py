ac0e2aa0fec1b0e896624b8c04c940010a847dad
Dario Castaño <dario.castano@aim.com>
2019-10-16 20:55:31 -0500

    Add empty end to print on 2-read-lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -12,7 +12,7 @@ def read_lines(filename="", nb_lines=0):
     file_lines = number_of_lines(filename)
     with open(filename, 'r', encoding='utf-8') as file:
         if nb_lines <= 0 or nb_lines > file_lines:
-            print(file.read())
+            print(file.read(), end="")
         else:
             for i in range(nb_lines):
                 print(file.readline(), end="")
