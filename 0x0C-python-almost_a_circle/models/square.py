a846cbd9e8a6e5f56b30466bd0498db41bf15db9
Dario Castaño <dario.castano@aim.com>
2019-10-20 19:12:35 -0500

    Update square class and tests

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.size,
+        )
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if args is None or not args:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        else:
+            valid_args = args[:4]
+            args_list = ['id', 'size', 'x', 'y']
+            for index in range(len(valid_args)):
+                setattr(self, args_list[index], valid_args[index])
