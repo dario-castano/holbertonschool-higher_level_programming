2c8394c502cb749b27fba172f872955a983208b6
Dario Castaño <dario.castano@aim.com>
2019-11-26 21:53:08 -0500

    Fix 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- count shows by genre
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS count_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS count_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
