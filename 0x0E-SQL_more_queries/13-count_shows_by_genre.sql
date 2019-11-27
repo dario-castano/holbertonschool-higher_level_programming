ac4fe86120e663a6e08fa782bbade9498dad8353
Dario Castaño <dario.castano@aim.com>
2019-11-26 21:56:03 -0500

    Rewrite 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- count shows by genre
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS count_shows
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY count_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS show_count 
+FROM tv_genres, tv_show_genres, tv_shows 
+WHERE tv_genres.id=tv_show_genres.genre_id and tv_shows.id = tv_show_genres.show_id 
+GROUP BY tv_genres.name 
+ORDER BY show_count DESC;
