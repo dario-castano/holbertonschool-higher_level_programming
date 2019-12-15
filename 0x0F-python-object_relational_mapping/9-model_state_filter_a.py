45e91e6b2480492092cb3a12e5c45a871c7616a9
Dario Castaño <dario.castano@aim.com>
2019-12-15 06:11:01 +0000

    Add task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+prints the first State object
+from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    db_conf = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': sys.argv[1],
+        'passwd': sys.argv[2],
+        'db': sys.argv[3],
+    }
+
+    address = 'mysql://{}:{}@{}:{}/{}'.format(
+                    db_conf['user'],
+                    db_conf['passwd'],
+                    db_conf['host'],
+                    db_conf['port'],
+                    db_conf['db'])
+
+    engine = create_engine(address)
+    SMaker = sessionmaker(engine)
+    session = SMaker()
+    query = session.query(State).filter(State.name.contains('a'))
+
+    for elem in query:
+        print("{}: {}".format(elem.id, elem.name))
