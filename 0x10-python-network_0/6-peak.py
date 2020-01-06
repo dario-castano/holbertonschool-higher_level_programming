0c88425c2956926f7404e7f47fcf8988bac1f22f
Dario Castaño <dario.castano@aim.com>
2020-01-06 15:56:43 -0500

    Fix pep8 on 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,10 @@
 Find a peak in a list of ints
 """
 
+
 def find_peak(list_of_integers):
+    """Find the peak
+    """
     if (not list_of_integers) or (list_of_integers is None):
         return None
     else:
