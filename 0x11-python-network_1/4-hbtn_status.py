73e5b637f2dea021edfe1e196ec428f113a300ab
Dario Castaño <dario.castano@aim.com>
2020-01-13 15:38:01 -0500

    Remove wrong print on 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,6 @@ import requests
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     ans = requests.get(url)
-    print(ans.text)
     print('Body response:')
     print('\t- type: {}'.format(type(ans.text)))
     print('\t- content: {}'.format(ans.text))
