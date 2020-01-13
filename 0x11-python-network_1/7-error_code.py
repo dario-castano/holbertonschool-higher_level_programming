4c7ad0c47e652ebfcfc15b6ce2a4d37cbce2db80
Dario Castaño <dario.castano@aim.com>
2020-01-13 16:55:41 -0500

    Add task 7 answer

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
