6a83c6ed6d6e8a98902390b26f4001b0439e9187
Dario Castaño <dario.castano@aim.com>
2020-01-13 21:53:44 -0500

    Add task 10 answer

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import sys
+import requests
 
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    username = sys.argv[1] if sys.argv.__len__() >= 1 else ""
+    password = sys.argv[2] if sys.argv.__len__() >= 2 else ""
+    auth = (username, password)
+    response = requests.get(url, auth=auth).json()
+    if 'id' in response:
+        print(response.get('id'))
