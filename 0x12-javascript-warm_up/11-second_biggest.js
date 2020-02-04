35cc33536a1934da65436dd5929a75a2f3b7fb0c
Dario Castaño <dario.castano@aim.com>
2020-02-04 14:21:15 -0500

    Improve task 11 logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,16 +3,6 @@ const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  const sorted = args.sort().reverse();
-  for (let i = 0; i < sorted.length; i++) {
-    if (i === sorted.length - 1) {
-      console.log(sorted[i]);
-      break;
-    } else if (sorted[i + 1] < sorted[i]) {
-      console.log(sorted[i + 1]);
-      break;
-    } else {
-      continue;
-    }
-  }
+  const sorted = [...new Set(args.sort().reverse())];
+  console.log(sorted[1])
 }
