66348dc5ebf977a0c34d0bf0d3df52f86ee039db
Dario Castaño <dario.castano@aim.com>
2020-02-04 14:14:26 -0500

    Fix task 11 logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,16 @@ const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  console.log(args.sort().reverse()[1]);
+  const sorted = args.sort().reverse();
+  for (let i = 0; i < sorted.length; i++) {
+    if (i === sorted.length - 1) {
+      console.log(sorted[i]);
+      break;
+    } else if (sorted[i + 1] < sorted[i]) {
+      console.log(sorted[i + 1]);
+      break;
+    } else {
+      continue;
+    }
+  }
 }
