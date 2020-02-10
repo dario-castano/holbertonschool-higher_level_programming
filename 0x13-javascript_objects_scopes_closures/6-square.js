3eec5ea49e517196116341edd8d21f46f481bcbb
Dario Castaño <dario.castano@aim.com>
2020-02-10 13:31:46 -0500

    Add task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Sqr = require('./5-square');
+
+class Square extends Sqr {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) super.print();
+    else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
