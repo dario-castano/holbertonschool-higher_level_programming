cd9fd4af7908b80c276dc976dd344619ef741a7b
Dario Castaño <dario.castano@aim.com>
2020-02-10 15:48:54 -0500

    Fix semistandard on task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,11 +2,11 @@
 
 const { exec } = require('child_process');
 
-const argv = process.argv.slice(2)
+const argv = process.argv.slice(2);
 const afile = argv[0];
 const bfile = argv[1];
 const outfile = argv[2];
 
-const cmd = `cat ${afile} ${bfile} > ${outfile}`
+const cmd = `cat ${afile} ${bfile} > ${outfile}`;
 
-exec(cmd)
+exec(cmd);
