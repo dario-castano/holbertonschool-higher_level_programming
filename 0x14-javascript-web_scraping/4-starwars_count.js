795a4aa8887aa79164d5a6a61e7dd2db8f353442
Dario Castaño <dario.castano@aim.com>
2020-02-11 15:51:45 -0500

    Fix map/filter on task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-const uri = 'http://swapi.co/api/people/18';
+const uri = process.argv[2];
+const id = 'https://swapi.co/api/people/18/';
 const request = require('request');
 
 request(uri, (error, _, body) => {
-  if (!error) console.log(JSON.parse(body).films.length);
-  else console.log(error);
+  if (!error) {
+    const results = JSON.parse(body).results;
+    const out = results.map(x => x.characters).filter(x => x.includes(id));
+    console.log(out.length);
+  } else console.log(error);
 });
